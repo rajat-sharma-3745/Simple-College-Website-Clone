e932c905258d7b9869a2acf2023c42e8b3231361
Rajat Sharma <sharmarajat3745@gmail.com>
2023-03-27 18:42:39 +0530

    register issue

--- router.js
+++ router.js
@@ -12,7 +12,8 @@ require('dotenv/config');
 
 const storage=multer.diskStorage({
       destination:function(req,file,cb){
-cb(null,path.join(__dirname,"/public/userimages"));
+// cb(null,path.join(__dirname,"/public/userimages"));
+cb(null,"/public/userimages");
       },
       filename:function(req,file,cb){
             const imagename=Date.now()+'-'+file.originalname;
